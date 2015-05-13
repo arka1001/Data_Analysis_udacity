3a6c882e7ac654cc8271807623b5257f651b7417
Arka Roy <arka1001@gmail.com>
2015-05-13 09:14:17 -0400

    gradient descent using pandas

---
+++ gradient_descent.py
@@ -0,0 +1,47 @@
+import numpy
+import pandas
+
+def normalize_features(array):
+   """
+   Normalize the features in our data set.
+   """
+   mu = array.mean()
+   sigma = array.std()
+   array_normalized = (array-mu)/sigma
+
+   return array_normalized, mu, sigma
+
+
+def compute_cost(features, values, theta):
+    """
+    Compute the cost of a list of parameters, theta, given a list of features 
+    (input data points) and values (output data points).
+    """
+    m = len(values)
+    sum_of_square_errors = numpy.square(numpy.dot(features, theta) - values).sum()
+    cost = sum_of_square_errors / (2*m)
+
+    return cost
+
+
+def gradient_descent(features, values, theta, alpha, num_iterations):
+    """
+    Perform gradient descent given a data set with an arbitrary number of features.
+    """
+  
+    m = len(values)
+    cost_history = []
+    cost = compute_cost(features, values, theta)
+    cost_history.append(cost)
+
+    for i in range(0,num_iterations):
+        
+        predicted_values = numpy.dot(features, theta)
+        theta = theta + (1.0*alpha/m)*numpy.dot(values - predicted_values,features)
+        cost = compute_cost(features, values, theta)
+       
+        cost_history.append(cost)
+
+    return theta, pandas.Series(cost_history) 
+
+
